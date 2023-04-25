2edbef840a5b68ef31305d231e54ff86567bff5a
Derick Olson <olsonderick@gmail.com>
2023-04-24 21:46:41 -0400

    add name and pw messages

--- test-form-app/src/LoginComponent.tsx
+++ test-form-app/src/LoginComponent.tsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 
 
 function LoginComponent() {
-  const [message, setMessage] = useState('Not submitted');
+  const [message, setMessage] = useState('Logged Out');
   const [username, setUsername] = useState('user');
   const [password, setPassword] = useState('pw');
+  const [invalidUsername, setInvalidUsername] = useState(false);
+  const [invalidPassword, setInvalidPassword] = useState(false);
   const handleSubmit = async () => {
     const queryParams = new URLSearchParams({
       username,
@@ -12,13 +14,20 @@ function LoginComponent() {
     });
     const res = await fetch(`http://localhost:3000/authenticate?${queryParams}`)
     const json = await res.json();
-    console.log(json)
     if (json.message) {
       setMessage(json.message);
-    } else if (json.error) {
-      setMessage(json.error);
+      setInvalidUsername(false);
+      setInvalidPassword(false);
+    } else if (json.error === 'Invalid username') {
+      setInvalidUsername(true);
+      setInvalidPassword(false);
+    } else if (json.error === 'Invalid password') {
+      setInvalidPassword(true);
+      setInvalidUsername(false);
     } else {
       setMessage('Unknown error');
+      setInvalidUsername(false);
+      setInvalidPassword(false);
       console.log(json);
     }
   }
@@ -30,9 +39,22 @@ function LoginComponent() {
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
       <label htmlFor="username">Username</label>
-      <input type='text' id="username" placeholder="Username" defaultValue={username} onChange={e => setUsername(e.target.value)} />
+      {invalidUsername && <div style={{ color: 'red' }}>Invalid username</div>}
+      <input type='text' id="username"
+        placeholder="Username"
+        defaultValue={username}
+        onKeyDown={handleKeydown}
+        onChange={e => setUsername(e.target.value)}
+      />
       <label htmlFor="password">Password</label>
-      <input type="text" id="password" placeholder="Password" defaultValue={password} onKeyDown={handleKeydown} onChange={e => setPassword(e.target.value)} />
+      {invalidPassword && <div style={{ color: 'red' }}>Invalid password</div>}
+      <input type="text"
+        id="password"
+        placeholder="Password"
+        defaultValue={password}
+        onKeyDown={handleKeydown}
+        onChange={e => setPassword(e.target.value)}
+      />
       <button onClick={handleSubmit}>Submit</button>
       <div>Status: {message} </div>
     </div>
