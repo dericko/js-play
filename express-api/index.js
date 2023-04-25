08731e788b57940052f986a4fea76274b86404ac
Derick Olson <olsonderick@gmail.com>
2023-04-24 22:14:47 -0400

    really truly awesome yay

--- express-api/index.js
+++ express-api/index.js
@@ -29,7 +29,7 @@ app.get('/users', (req, res) => {
     res.send(usersWithEmails);
 });
 
-app.get('/data', (req, res) => {
+app.get('/nestedData', (req, res) => {
     // a nested array of objects with nested objects and strings and comma-separated lists of attributes
     const data = [
         {
@@ -73,7 +73,73 @@ app.get('/data', (req, res) => {
         }
     ];
     res.send(data);
-}
+});
+
+app.get('/stringData', (req, res) => {
+    // a long string of comma-separated words from the start of a book
+    const data = 'It, was, the, best, of, times, it, was, the, worst, of, times, it, was, the, age, of, wisdom, it, was, the, age, of, foolishness, it, was, the, epoch, of, belief, it, was, the, epoch, of, incredulity, it, was, the, season, of, Light, it, was, the, season, of, Darkness, it, was, the, spring, of, hope, it, was, the, winter, of, despair, we, had, everything, before, us, we, had, nothing, before, us, we, were, all, going, direct, to, Heaven, we, were, all, going, direct, the, other, way, in, short, the, period, was, so, far, like, the, present, period, that, some, of, its, noisiest, authorities, insisted, on, its, being, received, for, good, or, for, evil, in, the, superlative, degree, of, comparison, only';
+    res.send(data);
+});
+
+app.get('/testCGInvalid', (req, res) => {
+    const data = {
+        data: {
+          isValid: false,
+          c2dTransactionId: null,
+          c2dRecordLocator: null,
+          partnerOfferUrl: null,
+          existingP2PListing: null,
+          carDetails: null,
+          failedLookup: true,
+          lookupUnavailable: null,
+          rateLimitError: null,
+          vin: null
+        },
+        errors: []
+      }
+    res.send(data);
+});
+
+app.get('testCGValid', (req, res) => {
+    const data = {
+        data: {
+            isValid: true,
+            c2dTransactionId: '1234567890',
+            c2dRecordLocator: { id: '1234567890' },
+            partnerOfferUrl: 'https://www.google.com',
+            existingP2PListing: { id: '1234567890', status: 'ACTIVE' },
+            carDetails: {
+                vin: '1234567890',
+                year: '2019',
+                make: 'Honda',
+                model: 'Civic',
+                trim: 'EX',
+                mileage: 10000,
+                exteriorColor: 'Black',
+                interiorColor: 'Black',
+                engine: '4 Cylinder',
+                transmission: 'Automatic',
+                drivetrain: 'FWD',
+                fuelType: 'Gasoline',
+                doors: 4,
+                seats: 5,
+                description: 'This is a description',
+                images: ['https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'],
+                options: ['Sunroof', 'Backup Camera'],
+                features: ['Bluetooth', 'Navigation'],
+                condition: 'USED',
+                price: 20000
+            },
+            failedLookup: null,
+            lookupUnavailable: null,
+            rateLimitError: null,
+            vin: '1234567890'
+        },
+        errors: []
+    }
+    res.send(data);
+});
+
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
