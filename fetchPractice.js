722492ca5e7d5ac8df58dfb1269cc85f5fadc763
Derick Olson <olsonderick@gmail.com>
2023-04-26 08:49:22 -0400

    setup fetch practice problems

---
+++ fetchPractice.js
@@ -0,0 +1,13 @@
+// 1) fetch and print from API
+
+// 2) sort a list by nested attributes
+
+// 3) filter a list by nested attributes
+
+// 4) dedup + sort a comma-separated list
+
+// 5) function for valid parens
+
+// 6) find top K frequent elements
+
+// write using promises and async/await
